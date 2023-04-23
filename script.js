fabdf334f74b10afd76d464baefe9cdf66dc6858
Chaka246055 <100752252+Chaka246055@users.noreply.github.com>
2023-04-24 02:17:58 +0400

    complete

---
+++ script.js
@@ -0,0 +1,13 @@
+function play(user) {
+    let result = "";
+    if (user === 'rock') {
+        result = "tie";
+    }
+    if (user === 'paper') {
+        result = "win";
+    }
+    if (user === 'scissors') {
+        result = "lose";
+    }
+    alert("Computer chose rock, you " + result + "!");
+}
